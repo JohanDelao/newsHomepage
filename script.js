a6fcd58dbac8b0bf67fa04dd4b8d89d1c473abe9
Johan Delao <johandelao10@gmail.com>
2023-01-06 20:00:57 -0500

    finished desktop and mobile

---
+++ script.js
@@ -0,0 +1,11 @@
+mobileNavbar = document.getElementById("mobileNav");
+console.log(mobileNavbar)
+
+mobileNavOpen = () => {
+    console.log("hello")
+    mobileNavbar.style.display = "block";
+}
+mobileNavClose = () => {
+    mobileNavbar.style.display = "none";
+}
+
